6804a77167b32af3f2539bb28613308b1653de3e
root <mayes809@gmail.com>
2022-03-06 00:24:44 +0000

    Finshed problem 1 or part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,7 +3,13 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr_sum = 0
+  arr_len = arr.size
+
+  for i in 0..arr_len
+    arr_sum = arr_sum + arr[i].to_i
+  end
+  return arr_sum
 end
 
 def max_2_sum arr
